cf4d2b0d2486a780b6e712e129e97e883083c6fe
Pike <nghiavt1@arrowhitech.co>
2023-10-23 14:46:32 +0700

    filter function

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.set('view engine', 'pug');
+app.set('views', './views');
 
+var posts = [
+    {id:1, title: 'Nodejs cơ bản'},
+    {id:2, title: 'Học vuejs'},
+    {id:3, title: 'Javascrip'}
+]
 app.get('/', function(req, res){
-    res.send("Hello World");
+    res.render('index', { posts: posts });
+    console.log(1111);
+})
+
+app.get('/search', function(req, res){
+    var id = req.query.id;
+	var data = posts.filter(function(item){
+        return item.id === parseInt(id)
+    });
+	res.render('index', {
+		posts: data
+    });
 })
 
 app.listen(port, function(error){
